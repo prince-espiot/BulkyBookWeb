470b1fd7f4afd98b4da670a15d64fcda9fac3f28
prince-espiot <okumop@yahoo.com>
2025-01-06 11:08:03 +0200

    Feat: Download DevOps documents added!

---
+++ BulkyBookWeb/Controllers/DownloadDocumentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class DownloadDocumentsController : Controller
+    {
+        public IActionResult DownloadPdf()
+        {
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Documents","Prince Okumo devops.pdf");
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound("File not found.");
+            }else {
+                var fileBytes = System.IO.File.ReadAllBytes(filePath);
+                return File(fileBytes, "application/pdf", "Prince Okumo devops.pdf");
+            }
+            
+        }
+    }
+}
